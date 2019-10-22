649479fc4d2a20e3ffb8eac52a7487f75b80d0cd
Maxim Skryabin <dev@mskr.name>
2019-10-22 21:41:09 +0300

    Added sample habits

--- habitude/Managers/DataManager.swift
+++ habitude/Managers/DataManager.swift
@@ -40,23 +40,22 @@ class DataManager {
 extension DataManager {
   func getTestHabits() -> [Habit] {
     return [
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6")),
-      Habit(name: "Drinking water", icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "4e81e6"))
+      Habit(name: NSLocalizedString("Drink water", comment: ""), icon: HabitIcon(code: "icon_habit_water"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("Brush your teeth", comment: ""), icon: HabitIcon(code: "icon_habit_tooth"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("Meditate", comment: ""), icon: HabitIcon(code: "icon_habit_meditate"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("No junk food", comment: ""), icon: HabitIcon(code: "icon_habit_nojunkfood"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("No milk", comment: ""), icon: HabitIcon(code: "icon_habit_nomilk"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("Read a book", comment: ""), icon: HabitIcon(code: "icon_habit_book"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("Take pills", comment: ""), icon: HabitIcon(code: "icon_habit_takepills"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("Wake up early", comment: ""), icon: HabitIcon(code: "icon_habit_sunrise"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("Healthy food", comment: ""), icon: HabitIcon(code: "icon_habit_healthyfood"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("Play some music", comment: ""), icon: HabitIcon(code: "icon_habit_music"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("Feed your dog", comment: ""), icon: HabitIcon(code: "icon_habit_feeddog"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("Feed your cat", comment: ""), icon: HabitIcon(code: "icon_habit_feedcat"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("Clean up", comment: ""), icon: HabitIcon(code: "icon_habit_clean"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("No alcohol", comment: ""), icon: HabitIcon(code: "icon_habit_noalcohol"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("Go to gym", comment: ""), icon: HabitIcon(code: "icon_habit_gym"), color: HabitColor(code: "FE404F")),
+      Habit(name: NSLocalizedString("No coffee", comment: ""), icon: HabitIcon(code: "icon_habit_nocoffee"), color: HabitColor(code: "FE404F"))
     ]
   }
 }
